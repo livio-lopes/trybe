981b7e039440a7f506e1bbc6be23e8ae32a3bea0
Marunga <antonio.lvio760@gmail.com>
2022-06-15 17:18:02 -0300

    exercicio-extra05 resolvido

--- exercicio-extra05.js
+++ exercicio-extra05.js
@@ -2,3 +2,14 @@
  a estrutura de repetição for e retorne no formato: "A soma de 1 a 1000 é: x." */
 
  
+ function acumula1000(){
+    var acumulador = 0;
+    for(var i =1;i<=1000;i++){
+        acumulador = acumulador + i;
+    }
+
+    return `A soma de 1 a 1000 é: ${acumulador}.`
+ }
+
+var k = acumula1000();
+console.log(k);
