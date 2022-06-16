e785e49f51dca4d7b77d2e8183464836a44501f8
Marunga <antonio.lvio760@gmail.com>
2022-06-16 13:52:59 -0300

    exercicio-extra08 resolvido

--- exercicio-extra08.js
+++ exercicio-extra08.js
@@ -7,3 +7,18 @@ e que retorne uma mensagem de acordo com a pontuação obtida.
 - entre 51 e 70: MEDIANO
 - entre 71 e 90: ÓTIMO
 - acima de 91: EXCELENTE */
+
+
+function vesepassa(nota){
+    var conceito;
+    if(nota<=10){ conceito = "PÉSSIMO"}
+    if(nota> 10 && nota <=30){ conceito = "RUIM"}
+    if(nota> 30 && nota <=50){ conceito = "REGULAR"}
+    if(nota> 50 && nota <=70){ conceito = "MEDIANO"}
+    if(nota> 70 && nota <=90){ conceito = "ÓTIMO"}
+    if(nota> 90){ conceito = "EXCELENTE"}
+    return conceito;
+}
+
+var fulano = vesepassa(50);
+console.log(`Fulano recebeu conceito: ${fulano}`);
