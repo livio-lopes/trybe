7c6606fc78228ec0558dba35f6d70af5a5432cd6
Marunga <antonio.lvio760@gmail.com>
2022-06-19 21:34:05 -0300

    exercicio-extra11 resolvido

--- exercicio-extra11.js
+++ exercicio-extra11.js
@@ -1,2 +1,18 @@
 /*11. Crie uma função que receba uma array de números e verifica 
 se cada um deles é divisível por 5. Se sim, armazene-os e retorne-os.*/
+
+
+function verSeMultiplo5(seq){
+    var tamanho = seq.length;
+    var so5 = [];
+    for(var i=0; i< tamanho;i++){
+        if(seq[i]%5 == 0){
+            so5.push(seq[i]);
+        }
+    }
+    return so5;
+}
+
+var shot = [5,25,30,65,40,682,24,26,23];
+
+console.log(verSeMultiplo5(shot));
