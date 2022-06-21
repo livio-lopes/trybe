ef2db8c93559680c3580c53bfcdf0c544528902e
Marunga <antonio.lvio760@gmail.com>
2022-06-21 14:31:49 -0300

    exercicio-extra16 resolvido

--- exercicio-extra16.js
+++ exercicio-extra16.js
@@ -1,2 +1,11 @@
 /*16. Escreva uma função que imprima os números de 1 (inclusive) 
 a 10 (inclusive) em ordem crescente. */
+
+
+function imprime(){
+    for(var i =1;i<=10;i++){
+        console.log(i);
+    }
+}
+
+imprime();
