07fc2aa08a0fe7e8b03ae157d0dde701239358a3
livio-lopes <antonio.lvio760@gmail.com>
2022-07-21 15:55:40 -0300

    d02-js exercicio-fix-02 resolvido

--- fundamentos-web/bl04-intro-javascript-logica-programacao/d02-array-loop-for/d02_exercicio_fix.js
+++ fundamentos-web/bl04-intro-javascript-logica-programacao/d02-array-loop-for/d02_exercicio_fix.js
@@ -1,6 +1,16 @@
-//Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+// 1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let number of numbers){
     console.log(number);
 }
+
+// 2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+let result = 0;
+
+for (let number of numbers){
+    result += number;
+}
+
+console.log(result);
