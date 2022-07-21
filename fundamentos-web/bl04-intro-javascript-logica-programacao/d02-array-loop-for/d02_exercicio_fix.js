e13faeae731fdcad37039c8651b10ca4a90b5ad2
livio-lopes <antonio.lvio760@gmail.com>
2022-07-21 16:04:22 -0300

    d02-js exercicio-fix-04 resolvido

--- fundamentos-web/bl04-intro-javascript-logica-programacao/d02-array-loop-for/d02_exercicio_fix.js
+++ fundamentos-web/bl04-intro-javascript-logica-programacao/d02-array-loop-for/d02_exercicio_fix.js
@@ -21,3 +21,12 @@ let arithmeticAverage = some/numbers.length;
 console.log(""); // Quebra de linha improvisada
 console.log(arithmeticAverage);
 
+// 4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+if (arithmeticAverage > 20){
+    console.log(""); // Quebra de linha improvisada
+    console.log("valor maior que 20");
+}else{
+    console.log(""); // Quebra de linha improvisada
+    console.log("valor menor ou igual a 20");
+}
